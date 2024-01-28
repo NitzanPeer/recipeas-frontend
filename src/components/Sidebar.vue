980d27588d64099f74923260d33d5aeed4c5fcda
Nitzan <prnitzan@gmail.com>
2024-01-28 17:49:41 +0200

    Fix mock data (new data with tag ids as objectid instead of strings within recipe obj). Switch tag remove btn to removal from rightclick on tag. Add fonts and improve css

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -4,10 +4,13 @@
     </button>
     <ul class="tag-list clean-list">
         <li class="tag-btn" v-for="tag in tags" :class="`tag-${tag._id}`" :key="tag.title">
-            <button class="tag-btn" :class="{ 'active-tag-btn': tag.isActive }" @click="toggleActiveTag(tag._id)">
+            <button class="tag-btn" :class="{ 'active-tag-btn': tag.isActive }"
+            @click="toggleActiveTag(event, tag._id)"
+            @contextmenu="showContextMenu($event, tag._id)"
+            >
                 {{ tag.title }}
+                <!-- <span class="delete-tag-btn fa-regular fa-trash-can" @click.stop.prevent="deleteTag(tag._id)"></span> -->
             </button>
-            <span class="delete-tag-btn fa-regular fa-trash-can" @click="deleteTag(tag._id)"></span>
             <!-- new font-awesome syntax that doesn't work: -->
             <!-- <font-awesome-icon class="delete-tag-btn" :icon="['far', 'trash-can']" /> -->
         </li>
@@ -38,7 +41,7 @@ export default {
         async getAllTags() {
             this.tags = await this.getTags
         },
-        async toggleActiveTag(tagId) {
+        async toggleActiveTag(event, tagId) {
             await this.toggleTag(tagId)
             await this.updateFilter({ tags: this.getActiveTagIds })
         },
@@ -55,6 +58,11 @@ export default {
                 await this.removeTag(tagId)
                 await this.resetTags()
             }
+        },
+        showContextMenu(event, tagId) {
+            event.preventDefault()
+            // In the future rightclick will open a context menu for other options other than delete
+                this.deleteTag(tagId)
         }
     },
     computed: {
